83c3b5ed366352cea154f8f469c1e81c4c94d44b
kwa090 <danny4yesu@gmail.com>
2023-07-27 21:11:34 +0000

    0x02-ES6_classes task

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,15 @@
+export default class Car {
+	constructor(brand, motor, color) {
+		this._brand = brand;
+		this._motor = motor;
+		this._color = color;
+		{
+	cloneCar() {
+	        const current = this;
+		return Object.assign(Object.create(Object.getPrototypeOf(current)), {
+			_brand: undefined,
+			_motor: undefined,
+			_color: undefined,
+			});
+		}
+}
